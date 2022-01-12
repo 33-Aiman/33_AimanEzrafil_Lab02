ad6deb056c5fb80a095a3db2210aa6476f6df3a9
33_AimanEzrafil <aiman_ezrafil@connect.ite.edu.sg>
2022-01-12 22:17:42 +0800

    Update GameController.cs
    Added 9 GameObjects Cube

--- 33_Aiman_Lab02/Assets/Scenes/GameController.cs
+++ 33_Aiman_Lab02/Assets/Scenes/GameController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject Cube0;
+    public GameObject Cube1;
+    public GameObject Cube2;
+    public GameObject Cube3;
+    public GameObject Cube4;
+    public GameObject Cube5;
+    public GameObject Cube6;
+    public GameObject Cube7;
+    public GameObject Cube8;
     // Start is called before the first frame update
     void Start()
     {
